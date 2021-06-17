ff6b952884fb1f7c94cf07f86af021861dce7c60
Andre Pierre <andre.laurence94@gmail.com>
2021-06-17 03:10:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end 
 
 def assign_rooms(attendees)
-  num = []
+  num = 0
   attendees.collect do |name|
     num += 1 
     return "Hello, #{name}! You'll be assigned to room #{num}!"
