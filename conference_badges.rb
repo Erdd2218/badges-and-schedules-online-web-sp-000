cf536694a098cf43beecfdf448c42b7526cb7eca
Andre Pierre <andre.laurence94@gmail.com>
2021-06-17 03:25:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,9 +20,9 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |name|
-     puts name
+     name
   end 
   assign_rooms.each(attendees) do |room|
-    puts room
+    room
   end
 end 
