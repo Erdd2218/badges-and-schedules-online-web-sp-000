07091c34a8b610d4b76b22f4cfcdfb1170f16322
Andre Pierre <andre.laurence94@gmail.com>
2021-06-17 03:15:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(attendees)
   num = 0
   attendees.collect do |name|
     num += 1 
-    "Hello, #{name}! You'll be assigned to room #{num}!"
+    return "Hello, #{name}! You'll be assigned to room #{num}!"
   end
 end
 
